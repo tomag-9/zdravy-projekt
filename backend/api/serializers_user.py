625f03023218d76a4f0827c90f031197e2949892
Tomas Magula <magula12@uniba.sk>
2026-02-21 17:58:02 +0200

    style: apply black formatting to serializers_user.py

--- backend/api/serializers_user.py
+++ backend/api/serializers_user.py
@@ -60,7 +60,10 @@ class AdminClientSettingsSerializer(serializers.ModelSerializer):
 class AdminUserSerializer(serializers.ModelSerializer):
     settings = serializers.SerializerMethodField()
     password = serializers.CharField(
-        write_only=True, required=False, allow_blank=True, style={"input_type": "password"}
+        write_only=True,
+        required=False,
+        allow_blank=True,
+        style={"input_type": "password"},
     )
 
     class Meta:
